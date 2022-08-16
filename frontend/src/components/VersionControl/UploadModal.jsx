c801c56b02dc8810809d29dd44eb1755e5af4a9e
Joe Shultz josephcshultz@gmail.com <josephcshultz@gmail.com>
2022-08-15 19:33:21 -0600

    merge fix

---
+++ frontend/src/components/VersionControl/UploadModal.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+class UploadModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      description: '',
+      url: ''
+    }
+  }
+
+  //for submission will send post request to the users versions
+  //function send post req and closes modal window
+  handleSubmit = () => {
+    //post req to server:
+
+    this.props.close();
+  }
+
+  render() {
+    return (
+      <div className="uploadModalBackground">
+        <div className="uploadModalContainer">
+          <div className="modalTopRow">
+            <div className="orangeText20">
+              upload new version
+            </div>
+            <button className="modalCloseButton" onClick={this.props.close}>
+              x
+            </button>
+          </div>
+          <div className="orangeText10">
+            <form>
+              <input className="forms" onChange={(e) => { this.setState({ name: e.target.value }) }}></input>
+            </form>
+            version name
+          </div>
+          <div className="orangeText10">
+            <form>
+              <input className="forms" onChange={(e) => { this.setState({ description: e.target.value }) }}></input>
+            </form>
+            description
+          </div>
+          <div className="orangeText10">
+            <form>
+              <input className="forms" onChange={(e) => { this.setState({ url: e.target.value }) }}></input>
+            </form>
+            url
+          </div>
+          <div>
+            <button className="modalSubmitButton" onClick={this.handleSubmit}>submit version</button>
+          </div>
+        </div>
+      </div >
+    )
+  }
+}
+
+export default UploadModal;
