eba4d2f4d2e35c7e5cad517936c2bae1c33c5ba1
Monica Cupp <monicaycupp@gmail.com>
2022-08-18 09:14:19 -0500

    Aug 18 Morning

--- frontend/src/components/profilepage/profilePageIndex.jsx
+++ frontend/src/components/profilepage/profilePageIndex.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from '../homepage/navbar.jsx';
 import PhotoSection from './userPhotoSection/userPhotoSection.jsx';
 import BioSection from './userBioSection/bioIndex.jsx';
+import profileObjOne from './userBioSection/bioData.jsx';
 
 const ProfileHome = () => {
   return (
     <>
     <Navbar />
     <PhotoSection />
-    <BioSection />
+    <BioSection {...profileObjOne}/>
 
 
     </>
--- frontend/src/components/profilepage/userBioSection/bioIndex.jsx
+++ frontend/src/components/profilepage/userBioSection/bioIndex.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Button} from 'react-scroll';
-import { BioContainer,
+import {Button} from '../profileComponents/buttonComponent.jsx';
+import {
+  BioContainer,
   BioWrapper,
   BioRow,
   Column1,
@@ -10,28 +11,32 @@ import { BioContainer,
   Subtitle,
   BtnWrp,
   Column2,
-  ImgWrp
+  ImgWrp,
+  Img,
 } from './bioElements.jsx';
 
-const BioSection = () => {
+const BioSection = (lightBg, id, imgStart, username, lightText, category, darkText, bio, buttonLabel, img, alt) => {
+  console.log('username: ', username)
   return (
     <>
-      <BioContainer Id={'Id'}>
+      <BioContainer lightBg={lightBg} id={id}>
         <BioWrapper>
-          <BioRow>
+          <BioRow imgStart={imgStart}>
             <Column1>
               <TextWrapper>
-                <TopLine>T-Jamz Biography</TopLine>
-                <Heading>Heading</Heading>
-                <Subtitle>Subtitle</Subtitle>
+                <TopLine>{username}</TopLine>
+                <Heading lightText={lightText}>
+                  {category}
+                </Heading>
+                <Subtitle darkText={darkText}>{bio}</Subtitle>
                   <BtnWrp>
-                    <Button to='home' />
+                    <Button to='home' >{buttonLabel}</Button>
                   </BtnWrp>
               </TextWrapper>
             </Column1>
             <Column2>
               <ImgWrp>
-                <Img/>
+                <Img src={img} alt={alt}/>
               </ImgWrp>
             </Column2>
           </BioRow>
