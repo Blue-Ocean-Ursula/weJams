db434a44be3046ade005c3b64ad96e08013a0a06
Monica Cupp <monicaycupp@gmail.com>
2022-08-18 11:55:38 -0500

    Aug 18 commit 11:55am for fresh start repo

--- frontend/src/components/profilepage/profilePageIndex.jsx
+++ frontend/src/components/profilepage/profilePageIndex.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Navbar from '../homepage/navbar.jsx';
 import PhotoSection from './userPhotoSection/userPhotoSection.jsx';
 import BioSection from './userBioSection/bioIndex.jsx';
